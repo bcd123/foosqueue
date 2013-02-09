2fbff55573ef7b40f4cb960cd6e17ef339682ef0
Brad Davis <davis.bradley@gmail.com>
2013-02-09 00:03:48 -0500

    pass parameters on command line; learn parameters mode

--- elo.py
+++ elo.py
@@ -5,8 +5,6 @@ import argparse
 # these are global defaults and should not be changed unless we want
 # to re-write history
 pointsForNewPlayer = 500.0
-eloFormulaK = 50.0
-eloFormulaF = 1000.0
 
 def getPlayerPoints(player, pointsMap):
     if player in pointsMap:
@@ -23,14 +21,18 @@ def updatePlayerPoints(player, pointsToAdd, pointsMap):
 def getTeamPoints(team, pointsMap):
     return (getPlayerPoints(team[0], pointsMap) + getPlayerPoints(team[1], pointsMap)) / 2.0
 
-def computeEloPoints(gameResults):
+def computeEloPoints(gameResults, eloFormulaK, eloFormulaF, verbose=False):
     # maps player name to list representing that player's points history
     eloPointsByPlayer = {}
+
+    numberOfGames = 0
+    correctPredictionCount = 0
 
     # play each game, updating points appropriately
     for game in gameResults:
         if len(game) != 4:
-            print 'Skipping line: ', game
+            if verbose:
+                print 'Skipping line: ', game
             continue
 
         winners = game[:2]
@@ -38,22 +40,28 @@ def computeEloPoints(gameResults):
         winnersInitialTeamPoints = getTeamPoints(winners, eloPointsByPlayer)
         loosersInitialTeamPoints = getTeamPoints(loosers, eloPointsByPlayer)
 
+        numberOfGames += 1
+        if winnersInitialTeamPoints > loosersInitialTeamPoints:
+            correctPredictionCount += 1
+        
         eloFormulaD  = winnersInitialTeamPoints-loosersInitialTeamPoints
         eloFormulaWe = 1.0 / (10.0**(-eloFormulaD/eloFormulaF)+1)
         pointsExchanged = round(eloFormulaK * (1.0 - eloFormulaWe))
 
-        print '%s,%s (%d,%d;%d) beat %s,%s (%d,%d;%d) exchanging %d points' % (winners[0], winners[1], 
-                                                                               getPlayerPoints(winners[0], eloPointsByPlayer), getPlayerPoints(winners[1], eloPointsByPlayer),
-                                                                               winnersInitialTeamPoints, 
-                                                                               loosers[0], loosers[1], 
-                                                                               getPlayerPoints(loosers[0], eloPointsByPlayer), getPlayerPoints(loosers[1], eloPointsByPlayer),
-                                                                               loosersInitialTeamPoints, 
-                                                                               pointsExchanged) 
+        if verbose:
+            print '%s,%s (%d,%d;%d) beat %s,%s (%d,%d;%d) exchanging %d points' % (winners[0], winners[1], 
+                                                                                   getPlayerPoints(winners[0], eloPointsByPlayer), getPlayerPoints(winners[1], eloPointsByPlayer),
+                                                                                   winnersInitialTeamPoints, 
+                                                                                   loosers[0], loosers[1], 
+                                                                                   getPlayerPoints(loosers[0], eloPointsByPlayer), getPlayerPoints(loosers[1], eloPointsByPlayer),
+                                                                                   loosersInitialTeamPoints, 
+                                                                                   pointsExchanged) 
 
         [updatePlayerPoints(player, pointsExchanged, eloPointsByPlayer) for player in winners]
         [updatePlayerPoints(player, -pointsExchanged, eloPointsByPlayer) for player in loosers]
     
-    return eloPointsByPlayer
+    percentCorrectPredictions = float(correctPredictionCount) / numberOfGames
+    return eloPointsByPlayer, percentCorrectPredictions
 
 def loadGameResultsFromFile(fileName):
     """read data from input file; format should be lines like this,
@@ -71,17 +79,37 @@ def printLadder(eloPointsByPlayer):
     for k,v in sorted(eloPointsByPlayer.iteritems(), key=lambda (k,v): (-v[-1],k)):
         print '%s\t%04d\t%04d' % (k, len(v)-1, v[-1])
 
+def findBestEloParameters(gameResults):
+    bestAccuracySoFar = 0.0
+    for k in range(5,100,5):
+        for f in range(10,1500,10):
+            eloPointsByPlayer, accuracy = computeEloPoints(gameResults, k, f)
+            if accuracy >= bestAccuracySoFar:
+                bestAccuracySoFar = accuracy
+                print '%f k=%d, f=%d' % (bestAccuracySoFar, k, f)
+    
 def main(argv=None):
     if argv is None:
         argv = sys.argv
     
     parser = argparse.ArgumentParser(description='Compute Elo scores for foosball results')
     parser.add_argument("results_filename")
+    parser.add_argument("--elo_k", type=float, default=50.0)
+    parser.add_argument("--elo_f", type=float, default=1000.0)
+    parser.add_argument("-v", "--verbose", action="store_true")
+    parser.add_argument("--learn_parameters", action="store_true")
     args = parser.parse_args()
     
     gameResults = loadGameResultsFromFile(args.results_filename)
-    eloPointsByPlayer = computeEloPoints(gameResults)
-    printLadder(eloPointsByPlayer)
+
+    if args.learn_parameters:
+        findBestEloParameters(gameResults)
+    else:
+        eloPointsByPlayer, percentCorrectPredictions = computeEloPoints(gameResults, args.elo_k, args.elo_f, args.verbose)
+        print 'Rankings led to correct prediction in %d%% of games' % (100*percentCorrectPredictions)
+        printLadder(eloPointsByPlayer)
+
+
 
 if __name__ == "__main__":
     sys.exit(main())
